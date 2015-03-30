e32a803d1354624b85d4974e94b1d20ae5def7d4
Stepan Burguchev <Stepan.Burguchev@comindware.com>
2015-03-30 16:07:17 +0300

    initial commit

---
+++ GraphOperations/Program.cs
@@ -0,0 +1,19 @@
+﻿// ***********************************************************************
+// <author>Stepan Burguchev</author>
+// <copyright company="Comindware">
+//   Copyright (c) Comindware 2010-2015. All rights reserved.
+// </copyright>
+// <summary>
+//   Program.cs
+// </summary>
+// ***********************************************************************
+
+namespace Comindware.Database.Examples.GraphOperations
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
