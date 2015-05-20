e0f9875425d5bca4c5a4ec50355acf6d51488c2d
Peter Volynsky <pvolynkiy@gmail.com>
2015-05-20 12:13:14 +0300

    some comments

--- CustomBuiltins/LocalCodeBuiltins.cs
+++ CustomBuiltins/LocalCodeBuiltins.cs
@@ -9,6 +9,7 @@ namespace Comindware.Database.Examples.CustomBuiltins
 {
     public static class LocalCodeBuiltins
     {
+        //Builtin below process subject arguments and write output value to object.
         [Logics.Formulae.Builtin("http://comindware.com/examples/customBuiltins#Average")]
         public static bool Average(IEnumerator<QName> left, out QName result)
         {
@@ -25,12 +26,15 @@ namespace Comindware.Database.Examples.CustomBuiltins
                 }
             }
             result = (sum / count).CreateLiteral();
-            return true; //always returns true - means that execution avareage will always succeed if arguments will be list.
+            // always returns true - means that execution avareage will always succeed if arguments match list criteria.
+            return true; 
         }
 
+        //Builtin below doesn`t provide any output except for testing for truth if left input is greater then right input.
         [Logics.Formulae.Builtin("http://comindware.com/examples/customBuiltins#isGreaterThan")]
         public static bool isGreaterThan(QName left, QName right)
         {
+            //test that left is greater then right and stop execution [return false;] if it is not thruth. 
             return left.GetValueOrDefault<decimal>() > right.GetValueOrDefault<decimal>();
         }
     }
